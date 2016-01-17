745ce31cb5ed334be6a4326c4d597f4d40908ee0
codeninja88 <jessica.mckenzie@my.jcu.edu.au>
2016-01-17 14:07:29 +1000

    attempt at adding login redirect

--- routes/login.js
+++ routes/login.js
@@ -9,8 +9,9 @@ loginRoute.get('/', function(req, res) {
 });
 
 
-loginRoute.post('/login', function(req, res) {
-    if (row.user_username === "test") {
+loginRoute.post('/', function(req, res) {
+    console.log(req.userLogin);
+    if (req.userLogin === "test") {
         res.render('pages/index');
     } else {
         res.render('pages/blog');
