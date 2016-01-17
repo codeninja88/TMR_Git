4be410bba0817295f0b9dfbb7a3b4ffbb277e56c
codeninja88 <jessica.mckenzie@my.jcu.edu.au>
2016-01-17 13:59:33 +1000

    attempt at adding login redirect

--- routes/login.js
+++ routes/login.js
@@ -2,14 +2,14 @@ var express = require('express');
 var loginRoute = express.Router();
 
 
-loginRoute.get('/', function(req, res) {
+loginRoute.get('/login', function(req, res) {
 
     res.render('pages/login');
 
 });
 
 
-loginRoute.post('/',
+loginRoute.post('/login', function(req, res) {
     if (row.user_username === "test") {
         res.render('pages/index');
     } else {
