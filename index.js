3a61cf58db44d258f1ca3866d53c898a19c1497b
codeninja88 <jessica.mckenzie@my.jcu.edu.au>
2016-01-17 12:38:48 +1000

    added blog page in basic form

--- index.js
+++ index.js
@@ -3,16 +3,27 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-// views is directory for all template files
+
+
+//all page templates / skeletons are stored within the views directory
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
+
+
+//PAGE ROUTING_________________________________________________________
+//Index / Home page
+app.get('/', function(req, res) {
+  res.render('pages/index');
+});
+
+//Blog page
+app.get('/blog', function(req, res) {
+  res.render('pages/blog');
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+  console.log('Site is running on port', app.get('port'));
 });
 
 
